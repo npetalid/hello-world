dfa2bb68d480c0e1ab2f436fec31a0e5c57b5331
Nikos Petalidis <nikos@petalidis.gr>
2023-12-12 21:00:00 +0200

    feat: hope  #29

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,6 @@ public class App
     {
         // Fix issue 1
         // Fix another issue
-        // Do something
 
         System.out.println( "Hello Nikos!" );
         System.out.println( "What a nice day!" );
