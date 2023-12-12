9d6d893cbcb5bd32fd7c6f2380f8d3c8150264d4
Nikos Petalidis <nikos@petalidis.gr>
2023-12-12 20:27:19 +0200

    fix: Forgotten something

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,7 +13,7 @@ public class App
         // Do something
         // Do something else
         // Fix 16
-        // Hello developer
+        // Hello 
         System.out.println( "Hello Nikos!" );
         System.out.println( "What a nice day!" );
 
