47892f71ea5663defaf88586e88100bbe15e8b81
Nikos Petalidis <nikos@petalidis.gr>
2023-12-22 14:13:22 +0200

    fix: Changed scode3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,7 @@ public class App
         // Fix another issue
         // change
         // This is it
-        System.out.println( "Hello Nikos now!" );
+        System.out.println( "Hello Nikos now1!" );
         System.out.println( "What a nice day!" );
 
 
