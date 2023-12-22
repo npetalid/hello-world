f1f209b10cfbcba31a4cfd6b900b1983a6f4b0a4
Nikos Petalidis <nikos@petalidis.gr>
2023-12-22 15:31:29 +0200

    fix: fix more1225

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,7 @@ public class App
         // Fix another issue
         // change
         // This is it
-        System.out.println( "Hello Nikos now-115a" );
+        System.out.println( "Hello Nikos now-116b" );
         System.out.println( "What a nice day!" );
 
 
